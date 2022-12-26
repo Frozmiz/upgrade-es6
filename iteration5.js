61e935cf8b388869b7a6f56594feaf4d46053674
Alejandro <frozmiz@live.com>
2022-12-26 17:20:58 +0100

    feat

--- iteration5.js
+++ iteration5.js
@@ -13,7 +13,7 @@ const agesMayorEdad = ages.filter(function (age) {
 
 // FORMA CORTA CON ARROW FUNCTION
 
-const greaterThan18 = ages.filter((ager) => ager >= 18);
+const greaterThan18 = ages.filter((age) => age >= 18);
 
 console.log(agesMayorEdad);
 console.log(greaterThan18);
@@ -22,24 +22,33 @@ console.log(greaterThan18);
 // con los valores que sean par.
 // const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
 
+const agesPair = ages.filter((age) => age % 2 == 0);
+
+console.log(agesPair);
+
 // 5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
 // con los streamers que tengan el gameMorePlayed = 'League of Legends'.
-// const streamers = [
-// 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-// 	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
-// 	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-// 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
-// ];
+
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const streamersLol = streamers.filter((streamer) => streamer.gameMorePlayed == "League of Legends");
+
+console.log(streamersLol);
 
 // 5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
 // con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos 
 // usar la funcion .includes() para la comprobación.
-// const streamers = [
-// 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-// 	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
-// 	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-// 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
-// ];
+
+const streamersWithU = streamers.filter((streamer) => {
+    return streamer.name.includes("u");
+});
+
+console.log(streamersWithU);
 
 // 5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
 // el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
@@ -47,29 +56,45 @@ console.log(greaterThan18);
 // Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
 // .age sea mayor que 35.
 
+const streamersLegends = streamers.filter((streamer) => {
+
+    const mayusString =  streamer.gameMorePlayed.includes("Legends");
+
+    if (mayusString && streamer.age > 35) {
+        streamer.gameMorePlayed = streamer.gameMorePlayed.toUpperCase();
+    }
+
+    return mayusString;
+});
+
+console.log(streamersLegends);
+
+
+
 // 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
 // los streamers que incluyan la palabra introducida en el input. De esta forma, si 
 // introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
 // introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
-// const streamers = [
-// 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-// 	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
-// 	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-// 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
-// ];
 
+const handleChange = (event) => {
+    console.log(event);
+    const streamerInput = streamers.filter((streamer) => 
+        streamer.name.toLowerCase().includes(input$$.value.toLowerCase())
+    );
+
+    console.log(streamerInput);
+};
+
+const input$$ = document.getElementById('toFilterStreamers');
+
+input$$.addEventListener("input", handleChange);
 
 // 5.7 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
 // los streamers que incluyan la palabra introducida en el input. De esta forma, si 
 // introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si introduzco 'i', 
 // me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
 // En este caso, muestra solo los streamers filtrados cuando hagamos click en el button.
-// const streamers = [
-// 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-// 	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
-// 	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-// 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
-// ];
+
 
 // <!doctype html>
 // <html lang="en">
@@ -84,3 +109,11 @@ console.log(greaterThan18);
 //   <button data-function="toShowFilterStreamers">Filter</button>
 // </body>
 // </html>
+
+
+
+// function muestraMensaje() {
+//     alert('Gracias por pinchar');
+//   }
+
+//   document.getElementById("pinchable").onclick = muestraMensaje;
